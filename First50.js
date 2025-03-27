e23d93a24fa5ef98a9e2df65d8ebf92954eeb985
IAMOTAS <niiotasalokome@gmail.com>
2025-03-27 12:18:30 +0000

    basic-js-for beginners

---
+++ First50.js
@@ -0,0 +1,11 @@
+//printing numbers
+//Print 1 to 10.
+for (var i = 1; i <= 10; i++ ){
+  console.log(i)
+}
+
+//printing odd numbers less than 100
+
+for (var i = 1; i < 100; i=+2 ){
+    console.log(i)
+}
